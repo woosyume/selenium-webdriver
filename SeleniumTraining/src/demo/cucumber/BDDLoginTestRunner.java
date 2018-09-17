559d577e3b66b81d12c40c5133db3adcb7fce79e
Woohyeok Kim, Aaron <woohyeok.kim@rakuten.com>
2018-09-17 23:58:26 +0900

    Runner impl is in progress

---
+++ SeleniumTraining/src/demo/cucumber/BDDLoginTestRunner.java
@@ -0,0 +1,12 @@
+package demo.cucumber;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features="features",glue = "demo.cucumber")
+public class BDDLoginTestRunner {
+
+}
